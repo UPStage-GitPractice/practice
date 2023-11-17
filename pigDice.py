6eb270f6bc42f47d713b1a396905c36036faef2a
HyeokHam <kn7137@gmail.com>
2023-11-17 18:07:33 +0900

    feat: Determine the winner

--- pigDice.py
+++ pigDice.py
@@ -1,1 +1,11 @@
 # pig dice program
+
+if User_Total > Computer_Total:
+    print("User Has Won")
+    print("The User Score has {}, and the Computer has Score {}".format(User_Total,Computer_Total))
+
+elif Computer_Total > User_Total:
+    print("Computer Has Won")
+    print("The User Score has {}, and the Computer has Score {}".format(User_Total,Computer_Total))
+else:
+    print("Match has Draw with User Score: {}, and the Computer Score: {}".format(User_Total,Computer_Total))
